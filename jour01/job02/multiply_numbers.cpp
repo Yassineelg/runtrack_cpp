da0b1ed30ee8b6db364d805993ea7d5ff915876d
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2024-04-06 16:58:18 +0200

    feat(jour01): Job02 - add multiplication program

---
+++ jour01/job02/multiply_numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    double num1, num2, product;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    product = num1 * num2;
+
+    std::cout << "The product of " << num1 << " and " << num2 << " is " << product << std::endl;
+
+    return 0;
+}
